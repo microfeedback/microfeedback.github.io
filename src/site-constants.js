9ce817430c9946825b0dd5ce287a92651d0f503f
Steven Loria <sloria1@gmail.com>
2018-01-21 08:27:14 +0800

    Update microfeedback backend URL

--- src/site-constants.js
+++ src/site-constants.js
@@ -4,7 +4,6 @@ export const urlRoot = 'https://microfeedback.github.io';
 export const githubOrgURL = 'https://github.com/MicroFeedback';
 export const githubURL =
   'https://github.com/MicroFeedback/microfeedback.github.io';
-// TODO
-export const microfeedbackURL = null;
+export const microfeedbackURL = 'https://microfeedback-github.now.sh/microfeedback/microfeedback.github.io';
 export const copyrightOwner = 'Steven Loria';
 export const copyrightOwnerURL = 'https://stevenloria.com';
