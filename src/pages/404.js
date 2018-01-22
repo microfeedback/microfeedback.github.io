7679c6a521cfc01133944324f19673ce9bf4a6b4
Steven Loria <sloria1@gmail.com>
2018-01-22 10:13:39 +0800

    Better 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import Container from '../components/Container';
+import TitleAndMetaTags from '../components/TitleAndMetaTags';
+import {name, githubURL} from '../site-constants';
+import {sharedStyles} from '../theme';
 
-const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+const issueURL = `${githubURL}/issues/new?title=Broken link`;
+
+const PageNotFound = () => (
+  <Container>
+    <div css={[sharedStyles.articleLayout.container, {marginTop: 60}]}>
+      <div css={sharedStyles.articleLayout.content}>
+        <h1>Page Not Found</h1>
+        <TitleAndMetaTags title={`${name} - Page Not Found`} />
+        <div css={sharedStyles.markdown}>
+          <p>We couldn&apos;t find what you were looking for.</p>
+          <p>
+            Please <a href={issueURL} target="_blank" rel="noopener noreferrer">post an issue</a>{' '}
+            about the broken link.
+          </p>
+        </div>
+      </div>
+    </div>
+  </Container>
 );
 
-export default NotFoundPage;
+export default PageNotFound;
